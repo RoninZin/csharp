58b91b813071143f10a7db334e71e39b7168cdf3
Roniberto Rodrigues Alves <ronirodrigues123@hotmail.com>
2023-08-28 14:59:45 -0300

    Aula 05 - Arrays

---
+++ aula_05/Exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            int numero;
+
+            Console.WriteLine("Digite 10 números inteiros não repetidos: ");
+
+            for (int contador = 0; contador < vetor.Length; contador++)
+            {
+                vetor[contador] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite o número que você deseja encontrar a posição: ");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (Array.IndexOf(vetor, numero) == -1 )
+            {
+                Console.WriteLine($"O número {numero} não foi encontrado!");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} está localizado na posição: " + Array.IndexOf(vetor, numero));
+            }
+           
+        }
+    }
+}
---
+++ aula_05/Exercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+
+            for (int indiceLinha = 0; indiceLinha < matriz.GetLength(0); indiceLinha++)
+            {
+                for (int indiceColuna = 0; indiceColuna < matriz.GetLength(1); indiceColuna++)
+                {
+                    Console.Write($"matriz[{indiceLinha} , {indiceColuna}] = ");
+                    matriz[indiceLinha, indiceColuna] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Elementos da Diagonal Principal: {matriz[0, 0]}, {matriz[1, 1]}, {matriz[2, 2]}");
+
+            Console.WriteLine($"\nElementos da Diagonal Secundária: {matriz[0, 2]}, {matriz[1, 1]}, {matriz[2, 0]}");
+
+            Console.WriteLine($"\nSoma dos Elementos da Diagonal Principal: "+ (matriz[0, 0]+matriz[1, 1]+matriz[2, 2]));
+
+            Console.WriteLine($"\nSoma dos Elementos da Diagonal Principal: " + (matriz[0, 2] + matriz[1, 1] + matriz[2, 0]));
+
+        }
+    }
+}
