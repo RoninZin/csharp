1f3baa73edc9975d5b3a34c99a39edfec214c3ae
Roniberto Rodrigues Alves <ronirodrigues123@hotmail.com>
2023-08-30 14:14:57 -0300

    Aula 07 - Métodos

---
+++ aula_07/Metodos_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using Metodos_02.Namespace01;
+
+namespace Metodos_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MetodoPublico();
+            MetodoInternal();
+            MetodoProtegido();
+            MetodoPrivado();
+
+            Classe01.MetodoPublico();
+            Classe01.MetodoInternal();
+            //Classe01.MetodoProtegido();
+            //Classe01.MetodoProtegido();
+
+            Classe02.MetodoPublico();
+            Classe02.MetodoPublico();
+            //Classe02.MetodoProtegido();
+            //Classe02.MetodoProtegido();
+
+        }
+
+        public static void MetodoPublico()
+        {
+            Console.WriteLine("Método Publico - Classe Program");
+        }
+
+        internal static void MetodoInternal()
+        {
+            Console.WriteLine("Método Internal - Classe Program");
+        }
+
+        protected static void MetodoProtegido()
+        {
+            Console.WriteLine("Método Protegido - Classe Program");
+        }
+
+        private static void MetodoPrivado()
+        {
+            Console.WriteLine("Método Privado - Classe Program");
+        }
+    }
+}
---
+++ aula_07/Métodos/Program.cs
@@ -0,0 +1,68 @@
+﻿using Metodos_02.Namespace01;
+
+namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero, numero2, divisao;
+
+            Console.WriteLine("Digite o primeiro número:");
+            numero = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"{numero} + {numero2} = " + Somar(numero, numero2));
+            Console.WriteLine($"{numero} - {numero2} = " + Subtrair(numero, numero2));
+            Console.WriteLine($"{numero} * {numero2} = " + Multiplicar(numero, numero2));
+            divisao = Dividir(numero, numero2);
+            Console.WriteLine(divisao == 0 ? "Não existe divisão por zero!" : $"{numero} / {numero2} = {divisao}");
+            Console.WriteLine($"{numero} ^ {numero2} = " + Potencia(numero , numero2));
+            Console.WriteLine($"Raiz quadrada de {numero} = " + Raiz(numero));
+
+            Dev();
+
+            Classe02.MetodoPublico();
+        }
+
+        static float Somar(float numero1, float numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        static float Subtrair(float numero1, float numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        static float Multiplicar(float numero1, float numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        static float Dividir(float numero1, float numero2)
+        {
+            if (numero2!=0)
+                return numero1 / numero2;
+            else
+                return 0;
+        }
+
+        static double Potencia(float numero1, float numero2)
+        {
+            return Math.Pow(numero1, numero2);
+        }
+
+        static double Raiz(float numero1)
+        {
+            return Math.Sqrt(numero1);
+        }
+
+        static void Dev()
+        {
+            Console.WriteLine("\nCriado por: Roniberto R.");
+        }
+    }
+}
